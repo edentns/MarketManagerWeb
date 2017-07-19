3f4646ce0fe9bba470233c048b2af615de671e0c
edentns <program_jhgf@edentns.com>
2017-07-19 17:01:09 +0900

    관리자 Dashboard pie chart 완성

--- WebContent/app/contents/01ma/ma01Dashboard/controllers/ma.Dashboard.ctrl.js
+++ WebContent/app/contents/01ma/ma01Dashboard/controllers/ma.Dashboard.ctrl.js
@@ -7,9 +7,124 @@
      * 코드관리
      */
     angular.module("ma.Dashboard.controller")
-        .controller("ma.DashboardCtrl", ["$scope", "$http", "$q", "$log", "ma.DashboardSvc", "APP_CODE", "$timeout", "resData", "Page",
-            function ($scope, $http, $q, $log, MaDashboardSvc, APP_CODE, $timeout, resData, Page) {
+        .controller("ma.DashboardCtrl", ["$scope", "$http", "$q", "$log", "ma.DashboardSvc", "APP_CODE", "$timeout", "resData", "Page", "UtilSvc", "ModalSvc",  
+            function ($scope, $http, $q, $log, MaDashboardSvc, APP_CODE, $timeout, resData, Page, UtilSvc, ModalSvc) {
+        		var vm = this;
 	            var page  = $scope.page = new Page({ auth: resData.access }),
-		            today = edt.getToday();
+		            today = edt.getToday(),
+		            defaultComponents=[
+		                {
+		                	visible     : true,
+		                	order       : 1,
+		                	name        : 'ma.JoinItemCtrl',
+		                	displayName : '가입상품',
+		                	templateURL : 'app/contents/01ma/ma01Dashboard/templates/ma.JoinItem.tpl.html'
+		                }
+		            ];
+	            
+	            vm.broadcast = function() {
+	            	$timeout(function() {
+	            		$scope.$broadcast('dashboard:query', {
+	            			tabName : vm.tab.activeTabNm,
+	            			code    : vm.code
+	            		});
+	            	}, 0);
+	            };
+	            
+	            vm.removeDashboardSetting = function() {
+	                var message = '설정된 정보를 초기화 하시겠습니까?';
+	                if (confirm(message)) {
+	                    UtilSvc.localStorage.removeItem(LOCAL_LIST_KEY);
+	                    vm.components = angular.copy(defaultComponents);
+	                    vm.broadcast();
+	                }
+	            };
+
+	            vm.openDashboardSetting = function() {
+	                var modalInstance = ModalSvc.openConfirmPopup({
+	                    templateUrl : 'app/contents/01ma/ma01Dashboard/templates/ma.DashboardSetting.modal.tpl.html',
+	                    controller  : 'ma.DashboardSettingModalCtrl',
+	                    resolve: {
+	                        payload: function() {
+	                            return {
+	                                data: vm.components
+	                            };
+	                        }
+	                    }
+	                });
+
+	                modalInstance.result.then(function(result) {
+	                    vm.components = result;
+	                    vm.broadcast();
+	                    UtilSvc.localStorage.setItem(LOCAL_LIST_KEY, vm.components);
+	                });
+
+	            };
+
+	            // 2016-08-02
+	            // -1-
+	            // 마지막으로 선택된 탭 값 불러오기.
+	            var activeTabName = UtilSvc.localStorage.getItem('activeTabName') || 'CHART';
+	            var tabs = [
+	                    ((!!activeTabName) && activeTabName === 'CHART'),
+	                    ((!!activeTabName) && activeTabName === 'TABLE')
+	                ];
+
+	            vm.tab  = {
+	                activeTabNm: activeTabName,
+	                tabs: [
+	                    { active: tabs[0], name: "CHART" },
+	                    { active: tabs[1], name: "TABLE" }
+	                ],
+	                changeTab: function ($e, tab) {
+	                    $e.preventDefault();
+
+	                    var me = this,
+	                        lastSelectedTab = '';
+
+	                    if (me.activeTabNm === tab.name) { return; }
+
+	                    me.tabs.forEach(function (o) {
+	                        o.active = false;
+	                    });
+	                    me.activeTabNm = tab.name;
+	                    tab.active = true;
+	                    
+	                    // 2016-08-02
+	                    // -1-
+	                    // 대쉬보드에서 마지막으로 선택한 탭 이름 저장.
+	                    UtilSvc.localStorage.setItem('activeTabName', tab.name);
+
+	                    vm.broadcast();
+
+	                    // 날짜 초기화
+	                    //me.initSearchPeriod();
+	                    //bigDeal.findBigDeal();  // 글로벌 탭 클릭 시 BIG DEAL은 조회를 해주어야한다.
+	                },
+	                equalsTabName: function (tabNm) {
+	                    return tabNm === this.activeTabNm;
+	                }
+	            };
+
+	            vm.components   = angular.copy(defaultComponents);
+	            
+	            vm.changeComponent = function() {
+	                vm.components[0].order = 2;
+	                vm.components[1].order = 1;
+	            };
+	            vm.removeComponent = function() {
+	                vm.components[0].visible = false;
+	            };
+	            vm.addComponent = function() {
+	                vm.components[0].order = 1;
+	                vm.components[1].order = 2;
+	                vm.components[0].visible = true;
+	                vm.broadcast();
+	            };
+
+
+	            page.bootstrap(function() {
+	                vm.broadcast();
+	            });
             }]);
 }());
---
+++ WebContent/app/contents/01ma/ma01Dashboard/controllers/ma.JoinItem.ctrl.js
@@ -0,0 +1,121 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name ma.Dashboard.controller : ma.DashboardCtrl
+     * 코드관리
+     */
+    angular.module("ma.Dashboard.controller")
+        .controller("ma.JoinItemCtrl", ['$scope', 'ma.DashboardSvc', '$timeout', 'UtilSvc', 
+            function ($scope, MaDashboardSvc, $timeout, UtilSvc) {
+	        $scope.$on('dashboard:query', function(event, payload) {
+	            MaDashboardSvc.setPayload(joinItem, payload);
+	            $timeout(function() {
+	            	joinItem.find();
+	            });
+	        });
+	
+	        var today   = edt.getToday(),
+	            period  = { startYear: today.y, startMonth: today.m, endYear: today.y, endMonth: today.m };
+	
+	        var joinItem = $scope.joinItem = {
+	            tabName     : '',
+	            code        : {},
+	            table       : {},
+	            chart       : {},
+	
+	            find: function() {
+	                var me      = this,
+	                    isChart = me.tabName === 'CHART',
+	                    target  = isChart ? joinItem.chart : joinItem.table;
+	                
+	                var param = {
+    					procedureParam : "USP_MA_01DASHBOARD01_GET&DTS_JOINREQSTRT@s|DTS_JOINREQEND@s",
+    					DTS_JOINREQSTRT: target.search.period.startYear+target.search.period.startMonth+"01000000",
+    					DTS_JOINREQEND : target.search.period.endYear+target.search.period.endMonth+"32000000"
+    				};      
+	            	
+        			UtilSvc.getList(param).then(function (res) {
+        				target.data = res.data.results[0];
+        				if(res.data.results[0].length > 0) {
+	        				if(isChart) {
+	        					res.data.results[1].forEach(function (data) {
+	        						data.CNT_001 = Number(data.CNT_001);
+	        						data.CNT_002 = Number(data.CNT_002);
+	        						data.CNT_003 = Number(data.CNT_003);
+	        						data.TOT_CNT = Number(data.TOT_CNT);
+	        					});
+	        					target.config.data(res.data.results[1]);
+	        				}
+        				}
+        			});
+	            }
+	        };
+	
+	        // TABLE
+	        joinItem.table = {
+	            search  : {
+	                period      : angular.extend({ type: 'current' }, angular.copy(period))
+	            },
+	            total   : {
+	                CNT_001 : 0,
+	                CNT_002 : 0,
+	                CNT_003 : 0
+	            },
+	            data    : {}
+	        };
+	
+	        // CHART
+	        joinItem.chart = {
+	            search: {
+	                field       : 'TOT_CNT',
+	                period      : angular.extend({ type: 'current' }, angular.copy(period)),
+	                changeSelectedData: function() {
+	                	var chart = $("#joinitemchart").data("kendoChart");
+	                	chart.options.series[0].field = joinItem.chart.search.field;
+	                	chart.refresh();
+	                }
+	            },
+	            data    : {}
+	        };
+	        joinItem.chart.options = {
+	        	legend: {
+	        		visible: false
+	        	},
+	        	seriesDefaults: {
+	        		labels: {
+	        			position: 'center',
+                		visible: true,
+                		background: 'transparent',
+                		rotation: -310,
+                		template: '#= category # #= value #건'
+	        		}
+	        	},
+	        	seriesColors: ['#03a9f4', '#ff9800', '#fad84a'],
+	        	tooltip: {
+	        		visible: true,
+                	template: '${ category } ${ value }건'
+	        	},
+                chartArea: {
+                	margin:{
+                		top:20,
+                		left:0,
+                		right:0,
+                		bottom:0
+                	}
+                },
+                plotArea: {
+                	margin:20
+                }
+	        };
+	        
+	        joinItem.chart.config = new kendo.data.DataSource({
+	        	transport: {
+	        		read: function(e) {
+	        			
+	        		}
+	        	}
+	        });
+        }]);
+}());
