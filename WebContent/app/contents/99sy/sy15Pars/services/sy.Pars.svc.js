928d46567eacabb5d6b6dccc6cf73cbb5c24e58c
HwangInhye <ddogi1234@gmail.com>
2017-09-21 16:04:31 +0900

    sy15Pars updated

--- WebContent/app/contents/99sy/sy15Pars/services/sy.Pars.svc.js
+++ WebContent/app/contents/99sy/sy15Pars/services/sy.Pars.svc.js
@@ -9,7 +9,16 @@
     angular.module("sy.Pars.service")
         .factory("sy.ParsSvc", ["APP_CONFIG", "$http", function (APP_CONFIG, $http) {
             return {
-            	
+            	/**
+                 * 마켓정보를 가져온다.
+                 * @returns {*}
+                 */
+                getMarketNameList: function () {
+                    return $http({
+                        method  : "GET",
+                        url		: APP_CONFIG.domain +"/sy15Pars"
+                    });
+                }
             };
         }]);
 }());
--- WebContent/app/contents/99sy/sy15Pars/sy.Pars.app.js
+++ WebContent/app/contents/99sy/sy15Pars/sy.Pars.app.js
@@ -8,19 +8,25 @@
 
     shpStdbyOrdApp.config(["$stateProvider", function ($stateProvider) {
         $stateProvider.state("app.syPars", {
-            url		: "/99sy/syPars?menu",
-            views	: {
+            url: "/99sy/syPars?menu",
+            views: {
             	contentView	: {
                     templateUrl	: "app/contents/99sy/sy15Pars/templates/sy.Pars.tpl.html",
                     controller  : "sy.ParsCtrl",
                     resolve		: {
-                        resData: ["AuthSvc", "$q", function (AuthSvc, $q) {
+                        resData: ["AuthSvc", "$q", "UtilSvc", function (AuthSvc, $q, UtilSvc) {
                             var defer 	= $q.defer(),
                                 resData = {};
 
                             AuthSvc.isAccess().then(function (result) {
                                 resData.access = result[0];
-                                defer.resolve(resData);
+                                var param = {
+            						procedureParam:"USP_SY_15PARS01_GET"
+            					};
+            					UtilSvc.getList(param).then(function (res) {
+            						resData.mngMrkData = res.data.results[0];
+            						defer.resolve(resData);
+            					});
                             });
 
                             return defer.promise;
