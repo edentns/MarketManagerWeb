f1fccb6093be352e4c4f6b9d3487c39a16daf01b
edentns <program_jhgf@edentns.com>
2017-10-17 17:11:07 +0900

    서버주소 수정

--- WebContent/app/contents/03sa/sa08ShpSbOrdImp/controllers/sa.ShpSbOrdImp.ctrl.js
+++ WebContent/app/contents/03sa/sa08ShpSbOrdImp/controllers/sa.ShpSbOrdImp.ctrl.js
@@ -7,8 +7,8 @@
      * 
      */
     angular.module("sa.ShpSbOrdImp.controller")
-        .controller("sa.ShpSbOrdImpCtrl", ["$scope", "$cookieStore", "$http", "$q", "$log", "$state", "sa.ShpSbOrdImpSvc", "APP_CODE", "$timeout", "resData", "Page", "UtilSvc", "MenuSvc",  "Util03saSvc", 
-            function ($scope, $cookieStore, $http, $q, $log, $state, saShpSbOrdImpSvc, APP_CODE, $timeout, resData, Page, UtilSvc, MenuSvc, Util03saSvc) {
+        .controller("sa.ShpSbOrdImpCtrl", ["$scope", "$cookieStore", "$http", "$q", "$log", "$state", "sa.ShpSbOrdImpSvc", "APP_CODE", "$timeout", "resData", "Page", "UtilSvc", "MenuSvc",  "Util03saSvc", "APP_CONFIG",  
+            function ($scope, $cookieStore, $http, $q, $log, $state, saShpSbOrdImpSvc, APP_CODE, $timeout, resData, Page, UtilSvc, MenuSvc, Util03saSvc, APP_CONFIG) {
 	            var page  = $scope.page = new Page({ auth: resData.access }),
 		            today = edt.getToday(),
 		            menuId = MenuSvc.getNO_M($state.current.name);
@@ -41,7 +41,8 @@
 	                    	valuePrimitive: true
 	    				},    				
 		        		shipCodeTotal : resData.parsDataSource,
-		        		ordStatusOp : []
+		        		ordStatusOp : [],
+		        		url : APP_CONFIG.domain
 		            };
 		            
 		            shpbyordDataVO.isOpen = function (val) {
