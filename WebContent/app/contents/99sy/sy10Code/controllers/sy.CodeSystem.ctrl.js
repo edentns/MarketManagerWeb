3c97238b660789cfa3eee789c098594232a3db18
hahm <hahm@192.168.8.161>
2017-10-18 16:11:21 +0900

    템플릿, 회사정보 colunm 속성 : batch 로 변경

--- WebContent/app/contents/99sy/sy10Code/controllers/sy.CodeSystem.ctrl.js
+++ WebContent/app/contents/99sy/sy10Code/controllers/sy.CodeSystem.ctrl.js
@@ -39,15 +39,15 @@
             	navigatable: true,
             	columns: [
     		           { field : "NM_DEF", title: "구분코드명", width: "160" },
-    		           { field : "DC_RMK1", title: "비고1", width: "100" },
-                       { field : "DC_RMK2", title: "비고2", width: "100" },
-                       { field : "DC_RMK3", title: "비고3", width: "100" },
-                       { field : "DC_RMK4", title: "비고4", width: "100" },
-                       { field : "DC_RMK5", title: "비고5", width: "100" },
-                       { field : "YN_USE", title: "사용여부", width: "120", cellClass: "ta-c", cellFilter: "ynUse", editableCellTemplate: "ui-grid/dropdownEditor", editDropdownValueLabel: "YN_USE",
+    		           { field : "DC_RMK1", title: "비고1", width: "90" },
+                       { field : "DC_RMK2", title: "비고2", width: "90" },
+                       { field : "DC_RMK3", title: "비고3", width: "90" },
+                       { field : "DC_RMK4", title: "비고4", width: "90" },
+                       { field : "DC_RMK5", title: "비고5", width: "90" },
+                       { field : "YN_USE", title: "사용여부", width: "100", cellClass: "ta-c", cellFilter: "ynUse", editableCellTemplate: "ui-grid/dropdownEditor", editDropdownValueLabel: "YN_USE",
                            editDropdownOptionsArray : [  { id: "Y", YN_USE: "사용" }, { id: "N", YN_USE: "사용안함" } ]
                        },
-                       { field : "DTS_UPDATE", title: "수정일시", width: "150", cellClass: "ta-c", enableCellEdit: false }
+                       { field : "DTS_UPDATE", title: "수정일시", width: "200", cellClass: "ta-c", enableCellEdit: false }
             	],
                 collapse: function(e) {
                     // console.log(e.sender);
