9a8ebf8fbc21c5c4e70b9c3dbb15a7327b32a25a
EDEN-HAHM <EDEN-HAHM@192.168.8.161>
2017-12-04 10:00:52 +0900

    sy_qa 단 임시 커밋

--- WebContent/app/contents/99sy/sy16Qa/services/sy.Qa.svc.js
+++ WebContent/app/contents/99sy/sy16Qa/services/sy.Qa.svc.js
@@ -10,10 +10,10 @@
         .factory("sy.QaSvc", ["APP_CONFIG", "$http", function (APP_CONFIG, $http) {
             return {
             	
-            	noticeInsert : function (param) {					
+            	qaInsert : function (param, CUD) {					
 					return $http({
 						method	: "POST",
-						url		: APP_CONFIG.domain +"/notice/noticeInsert",
+						url		: APP_CONFIG.domain +"/qa/qaInsert/"+CUD,
 						data	: param
 					});
 				}
