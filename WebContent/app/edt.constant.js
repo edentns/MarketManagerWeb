d5e095ffb8fedc4038437b795ce65272f80b97d2
edentns <program_jhgf@edentns.com>
2017-12-14 17:55:21 +0900

    암호화가 true가 되도록 수정

--- WebContent/app/edt.constant.js
+++ WebContent/app/edt.constant.js
@@ -7,7 +7,7 @@
         bsCd    : '00000',
 		domain  : '/MarketManagerServer', // 운영
         version : 'V 0.1.0',
-        encrypt : false 
+        encrypt : true 
     });
 
     edtApp.constant('APP_CODE', {
