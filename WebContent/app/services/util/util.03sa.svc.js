9d90fb92eefb2e4b5f4d59f9c22c147668b508dd
YUN <YUN@192.168.8.213>
2018-01-03 10:33:47 +0900

    1. 송장번호 유효성 검증 로직 변경 (정규식, 서버체크) 2. localstorage 로직 변경

--- WebContent/app/services/util/util.03sa.svc.js
+++ WebContent/app/services/util/util.03sa.svc.js
@@ -8,8 +8,8 @@
 	 */
 	angular.module('edtApp.common.service').service('Util03saSvc', ['$rootScope', '$state', '$window', '$http', '$timeout', 'APP_CONFIG', 'MenuSvc', 'UtilSvc',
 		function ($rootScope, $state, $window, $http, $timeout, APP_CONFIG, MenuSvc, UtilSvc) {
-			/*var user = $rootScope.webApp.user,
-				menu = $rootScope.webApp.menu;*/
+			var user = $rootScope.webApp.user,
+				menu = $rootScope.webApp.menu;
 						
 			//popup insert & update Validation
             this.readValidation = function(idx){
@@ -22,92 +22,31 @@
             	return result;
             };
             
-            //송장번호 유효성검사
+            //송장번호 유효성검사(정규식)
             this.NoINVOValidation = function(input, colunm, valicolunm){
-            	//var regTest = /^[0-9]{1}[0-9\-]+[0-9]{1}$/;
             	var regTest = /^(([\d]+)\-|([\d]+))+(\d)+$/;
             	var iValue = input.val().trim(); 
- 			    
+ 			                	
 			    if (input.is("[name='"+colunm+"']") && !iValue) {
                  	input.attr("data-"+valicolunm+"-msg", "송장번호를 입력해 주세요.");
                     return false;
                 };
-			    if(input.is("[name='"+colunm+"']") && iValue && (iValue.trim().length > 20 || iValue.trim().length < 8)){
-			    	input.attr("data-"+valicolunm+"-msg", "송장번호를 8자 이상 20자 이내로 입력해 주세요.");
-                    return false;
-			    };
-			    if (input.is("[name='"+colunm+"']") && iValue && !regTest.test(iValue.trim())) {
-					input.attr("data-"+valicolunm+"-msg", "송장 번호는 택배사에 따라 숫자 또는 숫자 와 '-'(짝대기, 연속으로 안됨) 조합으로 가능합니다.");
-				    return false;
-				};	
-				if (input.is("[name='"+colunm+"']") && iValue ) {
-					var row = input.closest("tr");
-					var grid = row.closest("[data-role=grid]").data("kendoGrid");					
-					var gridPop = angular.element(document.querySelector("div[kendo-grid]")).data("kendoGrid");
-					var dataItem = "";
-					var parsName = "";
-					var cdName = "";
-					
-					if(grid){
-						dataItem = grid.dataItem(row); 
-					}else if(gridPop){
-						dataItem = gridPop.dataItem($("[data-uid='" + input.closest("tr").parents("table").attr("data-uid") + "']", gridPop.table));
-					}						
-					
-					if(dataItem.CD_PARS || dataItem.CD_PARS_INPUT){
-						cdName = dataItem.CD_PARS || dataItem.CD_PARS_INPUT;
-						parsName = cdName.NM_PARS || dataItem.CD_PARS;
-						
-						if (parsName === "기타택배") {
-							var pattern1 = /^[0-9a-zA-Z]{9,12}$/i;
-							var pattern2 = /^[0-9a-zA-Z]{18}$/i;
-							var pattern3 = /^[0-9a-zA-Z]{25}$/i;
-							if(iValue.search(pattern1) === -1 && iValue.search(pattern2) === -1 && iValue.search(pattern3) === -1) {
-							   input.attr("data-"+valicolunm+"-msg", parsName+"의 운송장 번호 패턴에 맞지 않습니다.");
-							   return false;
-							}
-						} else if (parsName === "EMS") {
-							var pattern = /^[a-zA-Z]{2}[0-9]{9}[a-zA-Z]{2}$/;
-							if(iValue.search(pattern) === -1) {
-								input.attr("data-"+valicolunm+"-msg", parsName+"의 운송장 번호 패턴에 맞지 않습니다.");
-							    return false;
-							}
-						} else if (parsName === "한진택배" || parsName === "현대택배") {
-								if(!$.isNumeric(iValue)) {
-								    input.attr("data-"+valicolunm+"-msg", "운송장 번호는 숫자만 입력해주세요.");
-								    return false;
-								}else if( iValue.length != 10 && iValue.length != 12 ) {
-								    input.attr("data-"+valicolunm+"-msg", parsName+"의 운송장 번호는 10자리 또는 12자리의 숫자로 입력해주세요.");
-								    return false;
-								}
-						} else if (parsName === "경동택배") {
-							    if(!$.isNumeric(iValue)) {
-								    input.attr("data-"+valicolunm+"-msg", "운송장 번호는 숫자만 입력해주세요.");
-								    return false;
-							    }else if(iValue.length != 9 && iValue.length != 10 && iValue.length != 11) {
-								    input.attr("data-"+valicolunm+"-msg",parsName+"의 운송장 번호는 9자리 또는 10자리 또는 11자리의 숫자로 입력해주세요.");
-								    return false;
-							    }
-						} else if (parsName === "이노지스택배") {
-							    if(!$.isNumeric(iValue)) {
-								    input.attr("data-"+valicolunm+"-msg", "운송장 번호는 숫자만 입력해주세요.");
-								    return false;
-							    }else if(iValue.length > 13) {
-								    input.attr("data-"+valicolunm+"-msg",parsName+"의 운송장 번호는 최대 13자리의 숫자로 입력해주세요.");
-								    return false;
-							    }
-						} else if (parsName === "TNT Express") {
-								var pattern1 = /^[a-zA-Z]{2}[0-9]{9}[a-zA-Z]{2}$/;
-								var pattern2 = /^[0-9]{9}$/;
-								if(iValue.search(pattern1) === -1 && iValue.search(pattern2) === -1) {
-								   input.attr("data-"+valicolunm+"-msg", parsName+"의 운송장 번호 패턴에 맞지 않습니다.");
-								   return false;
-								}
-						};
-					}
-				}
+        		if (input.is("[name='"+colunm+"']") && iValue && !regTest.test(iValue.trim())) {
+ 					input.attr("data-"+valicolunm+"-msg", "송장번호는  숫자 또는 숫자와 '-'(특수문자) 조합으로만 가능합니다.");
+ 				    return false;
+ 				};
 				return true;
 			}; 
+			
+			//송장번호 유효성검사(서버)
+			this.noInvoAjaxValidation = function(param){
+				return $http({
+                    method  : "POST",
+					url		: APP_CONFIG.domain +"/parstest",
+					headers	: { "Content-Type": "application/x-www-form-urlencoded; text/plain; */*; charset=utf-8" },
+                    data     : $.param(param)
+                });
+			};
             
             this.popupUtil = {
             	blur : function(e){
@@ -121,6 +60,12 @@
             		if((regTest.test(inputText) && msg.length > 0) || (inputDate && msg.length > 0)){
             			msg.hide();
             		};
+            	},
+            	mblur : function(e){
+            		var element = $(e.currentTarget),
+        			msg = element.closest("tr").find(".k-invalid-msg") || element.closest("td").find(".k-invalid-msg");
+        			
+        			msg.hide();
             	}
             };
             
@@ -191,20 +136,25 @@
 			//로컬 스토리지			
 			this.localStorage = {
 				setItem: function(name, data) {
-					$window.localStorage.setItem(name, JSON.stringify(data));
-				},
-				
+					var key = user.NO_C+user.NO_EMP+name;
+					$window.localStorage.setItem(key, JSON.stringify(data));
+				},				
 				getItem: function(name) {
-					var result = $window.localStorage.getItem(name);
+					if (!user){
+						return '';
+					};
+					
+					var key = user.NO_C+user.NO_EMP+name,
+						result = $window.localStorage.getItem(key);
 					
 					if (result) {
 						result = JSON.parse(result);
 					}					
 					return result;
-				},
-                
-                removeItem: function(name) {                    
-                    $window.localStorage.removeItem(name);
+				},                
+                removeItem: function(name) {   
+					var key = user.NO_C+user.NO_EMP+name;               
+                    $window.localStorage.removeItem(key);
                 }
 			};
 		}
