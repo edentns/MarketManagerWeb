c7303500b4cfe88247e7f3ea3173c527a2013115
YUN <YUN@192.168.8.124>
2018-05-14 09:41:24 +0900

    날짜 조회 로직 추가

--- WebContent/app/services/util/util.svc.js
+++ WebContent/app/services/util/util.svc.js
@@ -621,6 +621,28 @@
 				
 				return ret;
 			};
+			
+			/**
+			 * date관련 함수
+			 * 날짜 비교 후 차이 일수 리턴
+			 */
+			this.diffDate = function (_date1, _date2) {
+			    var fParseDate = function(_arg){
+			    					var rpcUnder = _arg.replace('-','');
+			    					return rpcUnder.substring(0,4)+'-'+rpcUnder.substring(4,6)+'-'+rpcUnder.substring(6,8);
+			    				 };  
+			    var	diffDate_1 = _date1 instanceof Date ? _date1 : new Date(fParseDate(_date1)),
+			    	diffDate_2 = _date2 instanceof Date ? _date2 : new Date(fParseDate(_date2)),
+			    	diff = "";
+			 
+			    diffDate_1 = new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
+			    diffDate_2 = new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+			 
+			    diff = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+			    diff = Math.ceil(diff / (1000 * 3600 * 24));		
+			    
+			    return diff;
+			};
 
 			/**
 			 * 다운로드 관련 함수
