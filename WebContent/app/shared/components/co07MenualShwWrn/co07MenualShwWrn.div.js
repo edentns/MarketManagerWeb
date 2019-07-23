f3969d41893e01f8942f4179e6d6d3e9fe46d59c
yjyun <yjyun@edentns.com>
2019-07-23 15:18:56 +0900

    가독성

--- WebContent/app/shared/components/co07MenualShwWrn/co07MenualShwWrn.div.js
+++ WebContent/app/shared/components/co07MenualShwWrn/co07MenualShwWrn.div.js
@@ -15,6 +15,7 @@
 	            	  var deliMsg = "운송장이 등록되지 않았거나 업체에서 상품 준비중입니다.",
 	            	  	  noOrd = attrs.menualShwWrnNoOrd,
 	            	  	  lstYn = attrs.menualShwWrnListYn;
+	            	  
 	            	  var commonFnc = function(){
                 		  	var msg = scope.menualShwWrn;
             	  		  	  
@@ -26,19 +27,18 @@
                 		  		});
                 		  		element.find("div.k-invalid-msg").css("display","none");
 		                   	}
-            	  	  	  };
+            	  	  };
             	  		   
 	                  scope.$watch("menualShwWrn", function (newValue, oldValue) {
 	                	  var msg = newValue;
 	                	  	              	  
 	                	  angular.forEach(msg, function(item, index){
-	                		  //복수건
-	                		  //var trNoOrd = element.parent().find("td:eq(2)").text();
 	                		  var oldOrd = item.toString();
 	                		  
-	                		  if(noOrd === oldOrd && lstYn === 'Y'){		                		
-	                			  element.text(deliMsg);	
-	                		  }else if((!lstYn && oldOrd && !noOrd) || (!lstYn && oldOrd === noOrd)){                	
+	                		  if(noOrd === oldOrd && lstYn === 'Y'){
+	                			  element.text(deliMsg);
+	                		  }
+	                		  else if((!lstYn && oldOrd && !noOrd) || (!lstYn && oldOrd === noOrd)){                	
 		                		  var html = "<div class='k-widget k-tooltip k-tooltip-validation k-invalid-msg' " +
 		                		  		"style='margin: 0.5em; display: block;' data-for='NO_INVO' role='alert'><span class='k-icon k-i-warning'> " +
 		                		  				"</span>"+deliMsg+"<div class='k-callout k-callout-n'></div></div>";
